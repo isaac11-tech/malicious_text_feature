ca921194f335c20e29d90d809cafd19783a1f091
isaac tunik <itzchaktunik@gmail.com>
2025-08-21 11:06:24 +0300

    frist

---
+++ connection_db.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import os
+
+class ConnectionDB:
+    def __init__(self, db_name, collection_name):
+        mongo_uri_conn = os.getenv("CONN_STRING")
+
+        uri = "mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/"#test
+
+        # connecting to server
+        self.client = MongoClient(uri)
+        # select a DB
+        self.db = self.client[db_name]
+        # select collection
+        self.collection = self.db[collection_name]
+
+    def get_all_documents(self):
+        return list(self.collection.find())
+
+
+
+
+
+#testing
+c = ConnectionDB("IranMalDB","tweets")
+print(c.get_all_documents())
+
+#mongo_uri = os.getenv("CONN_STRING")
+#print("Mongo URI:", mongo_uri)
+
